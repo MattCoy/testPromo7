214014041adb2b0bdcbc03bd7f49258f25fc4527
MattCoy <contact@le-web-developpeur.com>
2018-06-10 10:17:07 +0200

    ajout formulaire de contact

--- contact.php
+++ contact.php
@@ -7,10 +7,14 @@
 	<h1>Contactez moi</h1>
 	<?php 
 	echo date('d/m/Y');
-	
+	//nouvelle fonctionnalité ajoutée via une branche	
 	if(isset($_POST['message'])){
 		echo 'message envoyé';
 	}
 	?>
+	<form method="post">
+		<input type="text" name="message">
+		<button type="submit">Envoyer</button>
+	</form>
 </body>
 </html>
